68f02657a8b61105460b6eb92e1edcb787c34840
Andrei Lavrenov <andrei.lavrenov@gmail.com>
2020-05-07 04:01:13 +0200

    created response.php
    created and implemented response.php across all files, cleaned up template.php and split it into regular and recursive.
    Also added functionality to upload.php to create directories if they are specified.

--- README.md
+++ README.md
@@ -1,15 +1,77 @@
-# stage
+# echo microservice
 
-A repository containing various projects created by Andrei Lavrenov during his internship at the Directorate of police information and ICT. 
+A repository containing various projects created by **Andrei Lavrenov** during his internship at the Directorate of police information and ICT. 
 
 ## response.php
-A helper class that's included with every microservice and generates a proper json response for any possible need. 
+should not be accessed directly, but if accessed will remain inert
 
-## echo | body.php
+A helper function that's included with every microservice and generates a proper json response for any possible need. 
+The function is called **generate_json_response($message, $data);**  both both variables being optional and defaulting to NULL. 
+
+A response is provided in the following format:
+```
+{
+    "response": {
+        "status": "OK",
+        "code": 200,
+        "message": $message
+    },
+    "data": "$data"
+}
+```
+
+
+## body.php
+requires POST
+
 When accessed, it returns the body of the received POST request. Any type of body is accepted, and responses try to be as close as possible to the originally received format and mime_type.
 
 It will deny any GET requests and empty POST bodies.
 
-if ?mode=json is specified, instead of returning the body with corresponding mime-type header, it will return a .json response with the data (whether it's text or binary) inside a json object.
+*not yet implemented:
+if ?mode=json is specified, instead of returning the body with corresponding mime-type header, it will return a .json response with the data, converted to base64 inside a json object. b64 is needed because json only support utf-8*
 
 
+## delete.php
+requires GET
+
+this allows the client to delete the specified folder or file from the ./templates directory. 
+The extension only has to be provided if the template is something other than json or extensionless file.
+```
+http://<server>/delete.php/<path>/<to>/<file>.<extension>
+```
+
+## upload.php
+requires POST
+
+allows the user to upload files to the ./templates directory using curl.
+```
+curl -F "uploaded_file=@my_file.txt" http://<server>/upload.php
+```
+allows specifying a directory inside the templates folder by extending the url.*
+eg: 
+```
+.../upload.php/<specific>/<directory>
+```
+will upload to 
+```
+./templates/specific/directory/my_file.txt
+```
+
+## list.php
+requires GET
+
+provides a response with an array of all available templates, directories and files inside the directories in json format.
+example:
+```
+response
+```
+
+## template.php
+Provides a response using an existing template from templates. 
+It is non recursive, so if you want the template to use your key value, it must be nested the same as in the template
+
+
+
+## template_recursive.php
+No matter where your key is, if the template has the same one, its value will be replaced by yours
--- src/public/delete.php
+++ src/public/delete.php
@@ -1,4 +1,6 @@
 <?php
+//include response generator
+include 'response.php';
 
 //An array of HTTP methods that we want to allow.
 $allowed_methods = array('GET');
@@ -10,9 +12,13 @@ $request_method = strtoupper($_SERVER['REQUEST_METHOD']);
 if(!in_array($request_method, $allowed_methods)) {
     
     //Send a 405 Method Not Allowed header.
-    header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
-    echo "You are using " . $request_method . "\r\n";
-    echo "Only GET is allowed at this endpoint \r\n";
+    // header($_SERVER["SERVER_PROTOCOL"]." 405 Method Not Allowed", true, 405);
+    http_response_code(405); //better and easier solution.
+
+    //create response
+    $message = "You are using " . $request_method . "\n Only GET is allowed at this endpoint.";
+    generate_json_response($message);
+
 
     exit; //Halt the script's execution.
 }
@@ -25,41 +31,61 @@ else {
     $path_relative = "templates/" . implode("/", $uri_relative);
     // echo $path_relative; echo "\r\n"; //testing $path_relative
     // echo $path_relative . ".json"; echo "\r\n"; //testing $path_relative
+    if(!empty($file_name)) {
 
-    if (file_exists($path_relative . ".json")) {
-        unlink($path_relative . ".json"); //delete the file
+        if(is_dir($path_relative)) {            
 
-        header('Content-Type: application/json');
-        $response = array("response" => 
-                                array("status" => "OK",
-                                      "code" => http_response_code(),
-                                      "message" => "{$file_name}.json has been deleted."));
+            //this is 5.2+ PHP code
+            $dir = $path_relative;
+            $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+            $files = new RecursiveIteratorIterator($it,
+                        RecursiveIteratorIterator::CHILD_FIRST);
+            foreach($files as $file) {
+                if ($file->isDir()){
+                    rmdir($file->getRealPath());
+                } else {
+                    unlink($file->getRealPath());
+                }
+            }
+            rmdir($dir);
 
-        echo json_encode($response);
-    }
-    else {
-        if (file_exists($path_relative)) {
-            unlink($path_relative); //delete the file
-
-            header('Content-Type: application/json');
-            $response = array("response" => 
-                                array("status" => "OK",
-                                      "code" => http_response_code(),
-                                      "message" => "{$file_name} has been deleted."));
-
-            echo json_encode($response);
+            $message = "folder '{$file_name}' has been deleted.";
+            generate_json_response($message);
         }
         else {
-            http_response_code(404);
-            header('Content-Type: application/json');
-            $response = array("response" => 
-                                    array("status" => "Not Found",
-                                          "code" => http_response_code(),
-                                          "message" => "Neither {$file_name}.json nor {$file_name} exist and thus cannot be deleted."));
-
-            echo json_encode($response);
+            if (file_exists($path_relative . ".json")) {
+                //delete the json file
+                unlink($path_relative . ".json"); 
+        
+                //create response
+                $message = "{$file_name}.json has been deleted.";
+                generate_json_response($message);
+            }
+            else {
+                if (file_exists($path_relative)) {
+                    //delete the file
+                    unlink($path_relative);
+        
+                    //create response
+                    $message = "{$file_name} has been deleted.";
+                    generate_json_response($message);
+                }
+        
+                else {
+                    //create response
+                    http_response_code(404);
+                    $message = "Neither {$file_name}.json nor {$file_name} exist and thus cannot be deleted.";
+                    generate_json_response($message);
+                }
+            }
         }
     }
+    else {
+        http_response_code(500);
+        $message = "You must provide a filename to be deleted.";
+        generate_json_response($message);
+    }
+    
 }
 
 ?>
